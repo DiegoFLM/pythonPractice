d4295af6005f54795d9a927f5203fcfca5e304eb
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 20:46:10 -0500

    c9:  dictsComprehensions.py ow

--- dictsComprehensions.py
+++ dictsComprehensions.py
@@ -1,9 +1,9 @@
 def run():
 
     keys = [i for i in range (1, 101)]
-    vals = [i**3 for i in range (1, 101)]
+    vals = [i**3 for i in range (1, len(keys) + 1)]
 
-    dic = {keys[k]: vals[k] for k in range (0, 100)}
+    dic = {keys[k]: vals[k] for k in range (0, len(keys))}
     print (dic)
 
 if __name__ == '__main__':
