6bcbceb638ce230024906b125ec8d19aa43afaf6
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 20:50:26 -0500

    c9 dictionary created with for loop

--- dictsComprehensions.py
+++ dictsComprehensions.py
@@ -1,10 +1,9 @@
 def run():
 
-    keys = [i for i in range (1, 101)]
-    vals = [i**3 for i in range (1, len(keys) + 1)]
-
-    dic = {keys[k]: vals[k] for k in range (0, len(keys))}
-    print (dic)
+    dict = {}
+    for i in range (1, 101):
+        dict[i] = i**3
+    print (dict)
 
 if __name__ == '__main__':
     run()
