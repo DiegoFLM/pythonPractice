e544270611ed9b0a35525b91e1ffc59a62b15f4f
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 20:55:29 -0500

    c9 dictionary comprehensions

--- dictsComprehensions.py
+++ dictsComprehensions.py
@@ -1,10 +1,7 @@
 def run():
 
-    dict = {}
-    for i in range (1, 101):
-        dict[i] = i**3
+    dict = {i : i**3 for i in range(1, 101) if (i % 3 != 0)}
     print (dict)
-
 if __name__ == '__main__':
     run()
 
