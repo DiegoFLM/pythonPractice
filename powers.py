f15018c00db0941077fbec8f6555a10d566ea486
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 20:23:15 -0500

    Py Int. c8. list comprehensions

--- powers.py
+++ powers.py
@@ -1,9 +1,15 @@
 def run():
     #Intermediate Python class 8.
-    powers = []
-    for n in range(1, 101):
-        powers.append( pow( n, 2) )
-    print ( powers )
+    #powers = []
+    #for n in range(1, 101):
+    #    if ( n % 3 == 0 ): continue
+    #    powers.append( pow( n, 2) )
+    #print ( powers )
+
+    squares = [i**2 for i in range (1, 101) if i % 3 != 0]
+    print (squares)
 
 if __name__ == '__main__':
     run()
+
+
