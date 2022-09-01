3b872f56aa4b49c3513979641341c4014b640cdb
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 21:06:44 -0500

    c9 naturals and sqrts in a dict

--- dictsComprehensions.py
+++ dictsComprehensions.py
@@ -1,6 +1,8 @@
+import math
+
 def run():
 
-    dict = {i : i**3 for i in range(1, 101) if (i % 3 != 0)}
+    dict = {i : round(math.sqrt(i), 2) for i in range(1, 1001)}
     print (dict)
 if __name__ == '__main__':
     run()
