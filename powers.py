c308cf7f499775bc9a857d836aaa4c702f4468d9
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-08-31 19:59:05 -0500

    Py Int. c8. powers.py

---
+++ powers.py
@@ -0,0 +1,9 @@
+def run():
+    #Intermediate Python class 8.
+    powers = []
+    for n in range(100):
+        powers.append( pow( n + 1, 2) )
+    print ( powers )
+
+if __name__ == '__main__':
+    run()
